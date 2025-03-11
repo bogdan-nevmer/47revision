11dcaad7dd06e2dbfd871c7ea10448dae43aa47f
bogdan nevmer <weckman689@gmail.com>
2025-03-11 18:17:47 +0300

    Add project files.

---
+++ 2/logic.cpp
@@ -0,0 +1,21 @@
+﻿// Среднее арифметическое ненулевых элементов
+// [The arithmetic mean of non - zero elements].
+//
+// Дан вектор целых чисел. Необходимо разработать функцию, 
+// которая вычисляет среднее арифметическое ненулевых элементов вектора.
+
+
+double calculate_arithmetical_mean_of_nonsero_elements(int* array, int size) {
+	int cnt = 0;
+	double avg=0;
+	for (int i = 0; i < size; ++i)
+	{
+		if (array[i] != 0) {
+			++cnt;
+			avg += array[i];
+		}
+	
+		avg /= cnt;
+	}
+	return avg;
+}
---
+++ 47revision/logic.cpp
@@ -0,0 +1,44 @@
+﻿// Сумма элементов [The sum of elements]
+//
+// Необходимо разработать функцию, которая вычисляет 
+// сумму элементов массива, расположенных между первыми
+// максимальным по модулю и минимальным по модулю элементами.
+#include "logic.h"
+
+
+
+int sum_between_first_min_max(int* array, int size){
+	int max = array[0];
+	int first_max_index = -1;
+	int min = array[0];
+	int first_min_index = -1;
+
+
+	
+	
+
+
+	
+	for (int i = 0; i < size; i++)
+	{
+		if (first_max_index == -1 || array[i] > max) {
+			first_max_index = i;
+		}
+	}
+
+
+	for (int i = 0; i < size; i++)
+	{
+		if (first_min_index == -1 || array[i] < min) {
+			first_min_index = i;
+
+		}
+	}
+
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += array[i];
+	}
+	return sum;
+}
