2a94a3eb3f188dd81ecc55093bea658685353b76
bogdan nevmer <weckman689@gmail.com>
2025-03-11 18:54:28 +0300

    update 4

--- 4/logic.cpp
+++ 4/logic.cpp
@@ -16,10 +16,8 @@ void count_local_min_and_max(int* array, int size, int* min_count, int* max_coun
 		if (array[i] < array[i - 1] && array[1] < array[i + 1]) {
 			min_count++;
 		}
-
-	}
-	for (int i = 1; i < size; i++)
-	{
+		else
+	
 		if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
 			max_count++;
 		}
