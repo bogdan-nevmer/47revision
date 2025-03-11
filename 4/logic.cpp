302e7591db065bf2fd3c47db9cd38570dfc13176
bogdan nevmer <weckman689@gmail.com>
2025-03-11 18:50:59 +0300

    make 4 task

--- 4/logic.cpp
+++ 4/logic.cpp
@@ -10,4 +10,19 @@
 void count_local_min_and_max(int* array, int size, int* min_count, int* max_count) {
 	*min_count = 0;
 	*max_count = 0;
+
+	for (int i = 1; i < size;++i)
+	{
+		if (array[i] < array[i - 1] && array[1] < array[i + 1]) {
+			min_count++;
+		}
+
+	}
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
+			max_count++;
+		}
+		
+	}
 }
